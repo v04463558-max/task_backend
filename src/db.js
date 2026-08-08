5d80dd397613808a6dde0d474c4868fe03098be5
vavz <v04463558@gmail.com>
2026-08-08 14:29:37 +0800

    update #3  nag crashed kasi

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 require("dotenv").config();
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
